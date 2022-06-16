48efa9eaa4946325588bc951c099dd051a24db8d
廖偉成 <s1075442@gm.pu.edu.tw>
2022-06-16 19:54:15 +0800

    first

---
+++ quiz410754427/Controllers/AboutController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace quiz410754427.Controllers
+{
+    public class AboutController : Controller
+    {
+        // GET: About
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Test()
+        {
+            return View();
+        }
+
+        
+        public ActionResult Count(string name, string number)
+        {
+            var result = " ";
+            if (number == "13")
+            {
+                result = "下午一點";
+            }
+            else if (number == "14") {
+                result = "下午-21點";
+            }
+
+            ViewBag.Name = name;
+            ViewBag.Result = result;
+            return View();
+        }
+    }
+}
